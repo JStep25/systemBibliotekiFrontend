7880c746737c4e9c970fcef1b5b839ff448df376
Jakub <jstepien257@gmail.com>
2026-04-29 16:56:59 +0200

    m

--- src/App.jsx
+++ src/App.jsx
@@ -2,50 +2,29 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Home from "./pages/Home"; // Przywrócony Home
 import AdminPanel from "./pages/AdminPanel";
 import UserPanel from "./pages/UserPanel";
-
-const HomeRedirect = () => {
-  const token = localStorage.getItem("token");
-  const userData = localStorage.getItem("user");
-
-  if (!token || !userData) {
-    return <Navigate to="/" replace />;
-  }
-
-  try {
-    const user = JSON.parse(userData);
-    
-    if (user.role === "admin") {
-      return <Navigate to="/admin/books" replace />;
-    }
-    return <Navigate to="/user/books" replace />;
-  } catch (err) {
-   
-    localStorage.clear();
-    return <Navigate to="/login" replace />;
-  }
-};
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         
+      
+        <Route path="/" element={<Home />} />
 
-        <Route path="/" element={<HomeRedirect />} />
-
-
+      
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
-
+      
         <Route path="/user/books" element={<UserPanel />} />
 
-
+       
         <Route path="/admin/books" element={<AdminPanel />} />
 
-
+       
         <Route path="*" element={<Navigate to="/" replace />} />
 
       </Routes>
